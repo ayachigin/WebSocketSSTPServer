75f6e8a66f1cbbad763d760142be3d085335e73b
Ayachi Gin <ayachigin@gmail.com>
2015-03-13 22:12:30 +0900

    Iroiro koushin!

--- src/Main.hs
+++ src/Main.hs
@@ -4,8 +4,10 @@ module Main where
 import Control.Monad (forever)
 import Data.Text (Text)
 import qualified Data.Text as T
+import Data.Text.Encoding (decodeUtf8)
 import qualified Network.WebSockets as WS
 import System.Environment (getArgs)
+import System.IO (hFlush, stdout)
 
 import Network.SSTP (simpleSSTP, emptyRequest, Request(..), fromRequest)
 
@@ -26,30 +28,35 @@ type Clients = [WS.Connection]
 myApp :: WS.ServerApp
 myApp pending = do
   con <- WS.acceptRequest pending
-  notify con
+  let path = decodeUtf8 . WS.requestPath . WS.pendingRequest$ pending
+  print path
+  sendSSTP path con
 
-sendText :: WS.Connection -> Text -> IO ()
-sendText con t = WS.sendTextData con t
+sendSSTP :: Text -> WS.Connection -> IO ()
+sendSSTP path conn = forever $ do
+  wsmsg <- fmap T.unpack . WS.receiveData $ conn
+  let msg = case path of
+             "/notify" -> notifyRequestString wsmsg
+             _         -> wsmsg
+  response <- simpleSSTP msg
+  putStrLn msg
+  putStrLn response
+  hFlush stdout
+  WS.sendTextData conn . T.pack $ response ++ "\r\n" ++ msg
 
-notify :: WS.Connection -> IO ()
-notify conn = forever $ do
-  msg <- WS.receiveData conn
-  let reqstr = buildRequest msg
-  r <- simpleSSTP reqstr
-  WS.sendTextData conn $ T.pack r
+notifyRequestString :: String -> String
+notifyRequestString = buildNotifyRequest . T.pack
 
-buildRequest :: Text -> String
-buildRequest msg = let params = map T.unpack . T.splitOn "," $ msg in
-                    fromRequest . req $ params
+buildNotifyRequest :: Text -> String
+buildNotifyRequest s = fromRequest request
   where
-    req params
-      | paramLen == 0 = emptyRequest
-      | paramLen == 1 = emptyRequest { _event=head params }
-      | paramLen == 2 = emptyRequest { _event=params!!0
-                                     , _script=params!!1 }
-      | otherwise     = emptyRequest { _event=params !! 0
-                                     , _script=params !! 1
-                                     , _reference=drop 2 params
-                                     }
-      where
-        paramLen = length params
+    args = map T.unpack . T.splitOn "," $ s
+    event      = if argLen >= 1 then args!!0 else _event emptyRequest
+    sender     = if argLen >= 2 then args!!1 else _sender emptyRequest
+    script     = if argLen >= 3 then args!!2 else _script emptyRequest
+    references = if argLen >= 4 then drop 3 args else _references emptyRequest
+    request = emptyRequest { _event=event
+                           , _sender=sender
+                           , _script=script
+                           , _references=references }
+    argLen = length args
